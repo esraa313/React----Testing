d2584e1b7848eb2a59372139550c8a1d427ede14
Esraa Hussein <esraahussein313@gmail.com>
2023-03-28 12:48:13 +0200

    Adding files

--- firsttest/src/App.js
+++ firsttest/src/App.js
@@ -2,10 +2,25 @@ import CardDetails from "./Components/CardDetails.js";
 import NavBar from "./Components/NavBar.js";
 
 
+const data = [
+  {title : "Title1" , description :"Description1"},
+  {title : "Title2" , description :"Description2"},
+  {title : "Title3" , description :"Description3"},
+  {title : "Title4" , description :"Description4"},
+  {title : "Title5" , description :"Description5"},
+  {title : "Title6" , description :"Description6"},
+  {title : "Title7" , description :"Description7"},
+]
 const App = () =>{
     return (
         <div>
+         {/* {
+        data.map((item) => {
+          return 
         
+
+        })
+      } */}
        <NavBar/>
       <CardDetails/>
         </div>
--- firsttest/src/Components/CardDetails.js
+++ firsttest/src/Components/CardDetails.js
@@ -1,49 +1,38 @@
 import React from 'react'
 import { Container , Row } from 'react-bootstrap';
-
 import { Button, Card } from "react-bootstrap"
 
-
+const data = [
+  {title : "Title1" , description :"Description1"},
+  {title : "Title2" , description :"Description2"},
+  {title : "Title3" , description :"Description3"},
+  {title : "Title4" , description :"Description4"},
+  {title : "Title5" , description :"Description5"},
+  {title : "Title6" , description :"Description6"},
+  {title : "Title7" , description :"Description7"},
+]
 const CardDetails = () => {
   return (
-    <Container>
+    
+    
+    <Container fluid>
       <Row>
-        <Card style={{ width: '18rem' }} className="col-lg-3">
+      {
+        data.map((item) => {
+          return <Card style={{ width: '18rem' }} className="col-lg-4">
           <Card.Img variant="top" src="logo192.png" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>{item.title}</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              {item.description}
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
         </Card>
-        
-        <Card style={{ width: '18rem' }} className="col-lg-3">
-          <Card.Img variant="top" src="logo192.png" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>  
 
-        <Card style={{ width: '18rem' }} className="col-lg-3">
-          <Card.Img variant="top" src="logo192.png" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </Row>
+        })
+      }
+     </Row>
     </Container>
   )
 }
